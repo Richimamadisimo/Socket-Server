43c2fdfe739680d5352f860ab341ce14ca207c53
Ricardo Uziel <ricky_uri@hotmail.com>
2021-04-30 09:47:07 -0500

    seccion 4

--- index.ts
+++ index.ts
@@ -1,17 +1,28 @@
-import Server from "./classes/server";
-import { SERVER_PORT } from "./global/environment";
-import  router  from './routes/router';
+import Server from './classes/server';
+import router from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const server = new Server();
 
-server.app.use(bodyParser.urlencoded({ extended: true }));
-server.app.use(bodyParser.json());
 
-server.app.use(cors({ origin: true, credentials: true }));
+const server = Server.instance;
 
-server.app.use('/', router)
+// BodyParser
+server.app.use( bodyParser.urlencoded({ extended: true }) );
+server.app.use( bodyParser.json() );
+
+// CORS
+server.app.use( cors({ origin: true, credentials: true  }) );
+
+
+// Rutas de servicios
+server.app.use('/', router );
+
+
+
+
 server.start( () => {
-     console.log(`Servidor corriendo en el puerto ${ SERVER_PORT }`);
- });
+    console.log(`Servidor corriendo en el puerto ${ server.port }`);
+});
+
+
